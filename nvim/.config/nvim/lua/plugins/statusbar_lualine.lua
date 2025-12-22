3b94f89dd0bf259146463c4ba2c1b2266c29bd25
Liam Coombs <liam@rocketscience.gg>
2025-12-22 13:17:45 +0000

    feat(nvim): lualine add relative path, time, LSP status

--- nvim/.config/nvim/lua/plugins/statusbar_lualine.lua
+++ nvim/.config/nvim/lua/plugins/statusbar_lualine.lua
@@ -6,6 +6,30 @@ return {
         require('lualine').setup {
             options = {
                 theme = 'auto',
+            },
+            sections = {
+                lualine_c = { { 'filename', path = 1 } },
+                lualine_x = {
+                    'lsp_status',
+                    function()
+                        local format_icons = {
+                            unix = '', -- e712
+                            dos = '', -- e70f
+                            mac = '', -- e711
+                        }
+                        local format = vim.bo.fileformat
+                        local icon = format_icons[format] or format
+                        if vim.bo.filetype == "" then
+                            return icon
+                        else
+                            return vim.bo.fileencoding .. "/" .. icon
+                        end
+
+                    end,
+                    'filetype',
+                    'progress'
+                },
+                lualine_y = { "os.date('%H:%M')" }
             }
         }
     end

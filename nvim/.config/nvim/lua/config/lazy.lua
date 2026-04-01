ffe41a1f76b9e8371ddf150326125f3201be5d91
Liam Coombs <LCCoombs@hotmail.co.uk>
2026-04-01 22:33:10 +0100

    fix(nvim): disable LazyVim import order check

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,3 +1,6 @@
+-- Disable some lazyvim check order
+vim.g.lazyvim_check_order = false
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
